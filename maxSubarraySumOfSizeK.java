cb0acedaf32544ed8f6967beda484ac5a7c8e0f1
SATYARTH <satyarthtiwari123@gmail.com>
2022-08-02 16:39:54 +0530

    Update maxSubarraySumOfSizeK.java

--- maxSubarraySumOfSizeK.java
+++ maxSubarraySumOfSizeK.java
@@ -1,13 +1,13 @@
 
 public class maxSubarraySumOfSizeK {
 	public static int maxWindowSumOfK(int arr[],int k) {
-		int i=0;
-		int j=0;
+		int i=0;	// start
+		int j=0;	//end
 		int maxSum=Integer.MIN_VALUE;
 		int currSum=0;
 		while(j<arr.length) {
 			currSum+=arr[j];
-			if(j-i+1<k) {
+			if(j-i+1<k) {          // window size will be ->  K = j-i+1
 				j++;
 			}
 		else if(j-i+1==k) {
@@ -22,11 +22,13 @@ public class maxSubarraySumOfSizeK {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[]= {2,5,1,8,2,9,1};
-		int k=3;
+		// BASIC SWT QUESTION 
+		// In this we have an array in which we need to find the max sum of subarrays of array of particualr length .i.e window.
+		int arr[]= {2,5,1,8,2,9,1};    // given array
+		int k=3;			// window size 
+		
 		int maxSum=maxWindowSumOfK(arr,k);
-		System.out.println(maxSum);
+		System.out.println(maxSum);  // max sum
 	}
 
 }
